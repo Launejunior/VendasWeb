4e91255ba536ca5a711355fec52b889ead9dfe76
Launejunior <launejunior@hotmail.com>
2024-02-03 17:02:13 -0300

    Primeiro Commit Department

---
+++ SalesWebMvc/Controllers/DepartmentsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models;
+using System.Collections.Generic;
+
+
+namespace SalesWebMvc.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Department> List = new List<Department>();
+            List.Add(new Department { Id = 1, Name = "Eletrônicos" });
+            List.Add(new Department { Id = 2, Name = "Perfumaria" });
+            return View(List);
+        }
+    }
+}
---
+++ SalesWebMvc/Controllers/TesteController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SalesWebMvc.Controllers
+{
+    public class TesteController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Saudacao"] = "Bem Vindo a Página TESTE";
+            ViewData["Email"] = "email@xpto.com";
+            return View();
+        }
+    }
+}
